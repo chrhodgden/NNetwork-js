52d9cb03004b00c0424d59a3b0ae897f827735be
Christian Hodgden <chrhodgden@gmail.com>
2024-02-09 14:40:06 -0600

    Update activationFunctions.test.js
    Added test constructor

--- test/unit_tests/activationFunctions.test.js
+++ test/unit_tests/activationFunctions.test.js
@@ -4,10 +4,15 @@ const math = require('mathjs');
 describe('Test Activation Function module', () => {
 
 	let result;
-	let testVector = [-2, -1, 0, 1, 2];
-	testVector = math.matrix(testVector);
-	let testMatrix = math.matrix([testVector, testVector]);
+	let testVector;
+	let testMatrix;
 	
+    beforeEach(() => {
+        testVector = [-2, -1, 0, 1, 2];
+    	testVector = math.matrix(testVector);
+        testMatrix = math.matrix([testVector, testVector]);
+	});
+    
 	result = activationFunctionList['sigmoid'].gx(testVector);
 	result = activationFunctionList['sigmoid'].dg_dx(testVector);
 	
